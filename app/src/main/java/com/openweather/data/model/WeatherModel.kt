189d40c743b5a33f46bfb9ab1911459d3f9137b5
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2023-09-25 03:49:27 -0500

    Add property with base URL for getting weather icons to WeatherModel data class

--- app/src/main/java/com/openweather/data/model/WeatherModel.kt
+++ app/src/main/java/com/openweather/data/model/WeatherModel.kt
@@ -2,6 +2,8 @@ package com.openweather.data.model
 
 import java.io.Serializable
 
+const val baseURLImage = "http://openweathermap.org/img/w/"
+
 data class WeatherModel(
     var main: String? = null,
     var description: String? = null,
