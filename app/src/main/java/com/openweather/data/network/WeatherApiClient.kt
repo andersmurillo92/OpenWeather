ba424ebfe06fbb5105f5dc5d1e2da04861751dd2
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2023-09-25 01:38:30 -0500

    Hardcode values on API client temporally

--- app/src/main/java/com/openweather/data/network/WeatherApiClient.kt
+++ app/src/main/java/com/openweather/data/network/WeatherApiClient.kt
@@ -7,9 +7,11 @@ import retrofit2.http.Query
 
 interface WeatherApiClient {
 
+    // TODO("Remove hardcoded values")
+
     @GET("forecast?")
-    fun getWeatherData(@Query("lat") latitude: String,
-                       @Query("lon") longitude: String,
-                       @Query("units") units: String,
+    fun getWeatherData(@Query("lat") latitude: String? = "4.6116516",
+                       @Query("lon") longitude: String? = "-74.1059474",
+                       @Query("units") units: String? = "imperial",
                        @Query("appid") appid: String = "078a420a8790df5a431a9faf14d61b67"): Response<ForecastResponseModel?>
 }
