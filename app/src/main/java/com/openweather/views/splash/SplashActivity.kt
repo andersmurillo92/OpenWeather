3a6db801df42a7073004fd81099a5c2f25cbeb15
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2023-09-24 20:35:31 -0500

    Add Dagger hilt library to project

--- app/src/main/java/com/openweather/views/splash/SplashActivity.kt
+++ app/src/main/java/com/openweather/views/splash/SplashActivity.kt
@@ -1,10 +1,10 @@
 package com.openweather.views.splash
 
-import android.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.WindowManager
